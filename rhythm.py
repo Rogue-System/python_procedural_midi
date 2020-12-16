c1063d43ff49915b6d243a4ac4083a8309abc4e7
Rogue-System <the.rogue.system@gmail.com>
2020-12-16 13:31:45 -0500

    updating ability to connect to midi devices

--- rhythm.py
+++ rhythm.py
@@ -1,3 +1,8 @@
-from midi import midicontroller
-default = serial0
-conn = midicontroller('dev' + str(raw_input(default)))
+from midi import MidiConnector
+serial_path = input("Please specify a serial input (i.e. serial0): ")
+def specify_midi_port(arg):
+    port = 'dev/' + str(arg)
+    return port
+    print(port)
+midi_device = specify_midi_port(serial_path)
+conn = MidiConnector(str(midi_device), timeout = 5)
