cdfd82966d73877432a4d05f10d6689d85d11537
Rogue-System <the.rogue.system@gmail.com>
2021-01-15 11:11:35 -0500

    Setting up keys as functions ans modes as classes.

--- rhythm.py
+++ rhythm.py
@@ -1,8 +1,24 @@
 from midi import MidiConnector
+
 serial_path = input("Please specify a serial input (i.e. serial0): ")
+
 def specify_midi_port(arg):
-    port = 'dev/' + str(arg)
+    port = '/dev/' + str(arg)
     return port
-    print(port)
+
 midi_device = specify_midi_port(serial_path)
 conn = MidiConnector(str(midi_device), timeout = 5)
+
+def major_scale(key)
+    pass
+def minor_scale(key)
+    pass
+class phyrigian
+    pass
+class ionian
+    pass
+class dorian
+    pass
+class locrian
+    pass
+    
